6b22e19575f05fd5a0b7fa0653c2933408f60b9e
Anthony Adamo <quic_aadamo@quicinc.com>
2023-01-05 14:55:38 -0800

    phone2: Add aidl bootcontrolhal to PRODUCT_PACKAGES
    add in the aidl bootcontrol hal server implementation to
    the PRODUCT PACKAGES
    
    Change-Id: I481e337b7d92d4a4d797a1e5dee695b39159f987
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -20,9 +20,8 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_client \
     update_verifier \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_PACKAGES += \
     checkpoint_gc \
