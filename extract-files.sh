02594202607a3374914603220e9e9a14866ac2c9
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-10-22 20:14:17 +0530

    fixup! phone2: Allow gettid syscall for wfdhdcphalservice
    Change-Id: I2a189383257a8e51d92da522bdbc4c13f7e01e54
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,7 @@ function blob_fixup() {
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d" "${2}"
             ;;
         vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+            [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
         vendor/lib64/libwvhidl.so)
