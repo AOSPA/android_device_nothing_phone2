1dd3afa9ea548234db9d8b813a13197ebca59d34
Omkar Chandorkar <gotenksIN@aospa.co>
2025-12-20 21:03:39 +0900

    phone2: Inherit NothingOS camera
    Change-Id: I081379ae5d0bef4dc745fec098ad8088c074a702
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- extract-files.py
+++ extract-files.py
@@ -111,14 +111,26 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/lib64/libmorpho_video_stabilizer.so': blob_fixup()
         .add_needed('libutils.so'),
-    ('vendor/lib64/libntcamallocator.so', 'vendor/lib64/vendor.noth.hardware.camera-service-impl.so'): blob_fixup()
-        .add_needed('libui_shim.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/nfc_nci_nxp_snxxx.so': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+    (
+        'vendor/lib64/libntcamallocator.so',
+        'vendor/lib64/vendor.noth.hardware.camera-service-impl.so',
+    ): blob_fixup().add_needed('libui_shim.so'),
+    'vendor/etc/init/vendor.noth.hardware.camera-service.rc': (
+        blob_fixup().regex_replace(r'\bNtCamAlgoCapacity\b', 'CameraServiceCapacity')
+    ),
+    (
+        'vendor/lib64/libntofflinepostproc.so',
+        'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so',
+    ): blob_fixup().replace_needed(
+        'vendor.qti.hardware.camera.postproc@1.0.so',
+        'vendor.qti.hardware.camera.postproc@1.0-nothing.so',
+    ),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
