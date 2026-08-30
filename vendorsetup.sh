07206a2e84fce6f711d7d7b93ddb2622346ae425
Omkar Chandorkar <gotenksIN@aospa.co>
2026-08-30 09:15:07 +0530

    phone2: Update to V4.1-260818-1726
    Change-Id: Iacfc25a7242b9c8213bf4bed20d2a466ebeebe94
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 # Shebang is intentionally missing - do not run as a script
 
-cat vendor/nothing/phone2/proprietary/vendor/lib64/libhyperzoom.arcsoft.so.part* > vendor/nothing/phone2/proprietary/vendor/lib64/libhyperzoom.arcsoft.so
+echo "2982b476fb72a170a27918ae0a0613b032bc56da251c34eca1fe59ef4904d7b1  vendor/nothing/phone2/proprietary/vendor/lib64/libhyperzoom.arcsoft.so" | sha256sum --status -c >/dev/null 2>&1 || cat vendor/nothing/phone2/proprietary/vendor/lib64/libhyperzoom.arcsoft.so.part* > vendor/nothing/phone2/proprietary/vendor/lib64/libhyperzoom.arcsoft.so
+
