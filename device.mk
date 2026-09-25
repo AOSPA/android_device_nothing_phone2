5ac606ed36aa29afee6bf8661fc0eaaf158c46c5
Omkar Chandorkar <gotenksIN@aospa.co>
2026-09-25 05:17:04 +0000

    phone2: Include audio extension hals
    - soundtrigger works now
    
    Change-Id: I89e1c87ea269679d73e059b8a736c03f2d8fd0cb
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_cape_qssi/audio_policy_configuration.xml \
     $(DEVICE_PATH)/configs/dirac/interface.json:$(TARGET_COPY_OUT_VENDOR)/etc/dirac/interface.json
 
+DEVICE_MANIFEST_FILE += \
+    vendor/qcom/opensource/audio-hal/primary-hal/configs/common/manifest_non_qmaa.xml \
+    vendor/qcom/opensource/audio-hal/primary-hal/configs/common/manifest_non_qmaa_extn.xml
+
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # AVB
